53f42e13c80ff3f04566e227ddb6959b5de67dbe
Zhuxb <ZhuxbClouds@gmail.com>
2022-03-04 19:56:09 +0800

    3.4更新 新增文章两篇

---
+++ public/js/myFunction.js
@@ -0,0 +1,4 @@
+let subtitle = document.getElementById('subtitle')
+let motto = ["解剑放舟清波上，不羡鸳鸯不羡仙。","不会飞的猪，就只是一只普通的猪而已。","他日我若为青帝，报与桃花一处开。"]
+subtitle.innerText = motto[Math.floor(Math.random()*3)]
+// console.log("Hello");
