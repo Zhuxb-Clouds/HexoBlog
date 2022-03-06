801a879d3fb8ca6ce8776324bca4ac00b70233e2
Zhuxb <ZhuxbClouds@gmail.com>
2022-03-06 20:09:31 +0800

    Site updated: 2022-03-06 20:09:29

--- js/myFunction.js
+++ js/myFunction.js
@@ -1,4 +1,8 @@
 let subtitle = document.getElementById('subtitle')
 let motto = ["解剑放舟清波上，不羡鸳鸯不羡仙。","不会飞的猪，就只是一只普通的猪而已。","他日我若为青帝，报与桃花一处开。"]
 subtitle.innerText = motto[Math.floor(Math.random()*3)]
-// console.log("Hello");
+// // console.log("Hello");
+
+// let postBg = document.getElementsByClassName('post_bg')
+
+//获取到postBg之后，根据postBg的图片生成缩略图
